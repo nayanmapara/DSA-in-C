26edfad06fba7c394cfe6176104ed6e512bd84c9
Nayan <contact@nayanm.me>
2024-01-11 01:30:44 -0500

    Added basic comments

--- Assignments/Assignment1/Tutorial1/Question3.c
+++ Assignments/Assignment1/Tutorial1/Question3.c
@@ -8,10 +8,11 @@ int main (void)
 {
    int number;
    
-   printf("number\tsquare\tcube\n");
+   printf("number\tsquare\tcube\n"); // Header
+
    for (int i = 0; i <= 10; i++)
    {
-    printf("%d\t%d\t%d\t\n", i, i * i, i * i * i);
+    printf("%d\t%d\t%d\t\n", i, i * i, i * i * i); // Body
    }
 
 }
