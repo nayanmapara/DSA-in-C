6278ba9296bc0b7d9894c88229f477278d226c46
Nayan <contact@nayanm.me>
2024-01-29 10:27:34 -0500

    Added tokenization

--- ClassWork/week4/Startup/P8_9_startup.c
+++ ClassWork/week4/Startup/P8_9_startup.c
@@ -7,6 +7,8 @@ int main( void )
    char string1[ 20 ]; // first string input by user
    char string2[ 20 ]; // second string input by user
    int result; // result of comparing two strings
+   char *token1 = NULL;
+   char *token2 = NULL;
 
    printf( "%s", "Enter two strings: \n" );
    // read two strings
@@ -15,17 +17,20 @@ int main( void )
    printf("Enter string 2: ");
    fgets( string2, 20, stdin);
    
-   result = strcmp( string1, string2); // use strcmp() to decide the if the strings are equal 
+   token1 = strtok(string1, "\n");
+   token2 = strtok(string2, "\n");
+
+   result = strcmp( token1, token2); // use strcmp() to decide the if the strings are equal 
   
    // display appropriate message for result
    if ( result > 0 ) {
-      printf( "\"%s\" is greater than \"%s\"\n", string1, string2 );
+      printf( "\"%s\" is greater than \"%s\"\n", token1, token2 );
    } // end if
    else if ( 0 == result ) {
-      printf( "\"%s\" is equal to \"%s\"\n", string1, string2 );
+      printf( "\"%s\" is equal to \"%s\"\n", token1, token2 );
    } // end else if
    else {
-      printf( "\"%s\" is less than \"%s\"\n", string1, string2 );
+      printf( "\"%s\" is less than \"%s\"\n", token1, token2 );
    } // end else
 } // end main
 
